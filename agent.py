70183d8178d563813cc67b0282a0e88bf275546b
MOHANGANDHI <malapati@cs.stonybrook.edu>
2018-12-02 17:41:13 -0500

    rl env changes

--- agent.py
+++ agent.py
@@ -1,27 +1,42 @@
+#TO DO : timeout decorator
+
+class State(object):
+    def __init__(self,state,my_id):
+        opponent_id = 2 if id==1 else 1
+        self.turn = state[0]
+        self.player_properties = state[1]
+        self.my_position = state[2][my_id]
+        self.opponent_position = state[2][opponent_id]
+        self.my_cash = state[3][my_id]
+        self.opponent_cash = state[3][opponent_id]
+        self.phase = state[4]
+        self.payload = state[5]
+        self.debt = state[6]
+        self.previous_states  = state[7]
+
 class Agent(object):
     def __init__(self, id=0):
         self.id = id  # Player 1 -> id=1, Player 2 ->id=2
 
-        def getBMSTDecision(self, state):
-            pass
+    def get_state_object(self,state):
+        return State(state,self.id)
 
-        def respondTrade(self, state):
-            pass
+    def getBMSTDecision(self, state):
+        pass
 
-        def buyProperty(self, state):
-            pass
+    def respondTrade(self, state):
+        pass(done)
 
-        def auctionProperty(self, state):
-            pass
+    def buyProperty(self, state):
+        pass(done)
 
-        def jailDecision(self, state):
-            pass
+    def auctionProperty(self, state):
+        pass
 
-        def respondTrade(self, state):
-            pass
+    def jailDecision(self, state):
+        pass(done)
+
 
         def receiveState(self, state):
-            pass
+            pass(done)
 
-        def respondMortgage(self, state):
-            pass
